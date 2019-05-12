377f679f12c8b5f1d99bf7f4f95d7bbc3b8796c9
REST-IRINA <materialdlymeny@mail.ru>
2019-05-12 13:00:00 +0300

    переписанный тест

--- src/main/java/MainClass.java
+++ src/main/java/MainClass.java
@@ -1,6 +1,13 @@
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
 public class MainClass {
 
     public static void main(String[] args) {
+        System.setProperty("webdriver.gecko.driver", "C:\\Project\\pageobjectinvitrotest\\Drivers\\geckodriver.exe");
+
+        WebDriver driver = new FirefoxDriver();
+        driver.get("https://www.invitro.ru");
 
     }
 }
--- src/test/java/MainPageTest.java
+++ src/test/java/MainPageTest.java
@@ -1,7 +1,8 @@
+
 import org.junit.Assert;
-import org.junit.Test;
-import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 
@@ -9,31 +10,41 @@ import java.util.concurrent.TimeUnit;
 
 public class MainPageTest {
 
-    private WebDriver driver;
-    private MainPage mainPage;
+    private static FirefoxDriver driver;
+    private static MainPage mainPage;
 
-    @BeforeAll
-    public void setUp (){
-        System.setProperty("webdriver.gecko.driver", "C:\\Project\\testselenium\\Drivers\\geckodriver.exe");
-        driver = new FirefoxDriver();
-        driver.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
-        driver.manage().window().maximize();
-        driver.get("https://www.invitro.ru");
-        mainPage = new MainPage(driver);
-    }
+
+   @BeforeAll
+   static void initAll() {
+       System.setProperty("webdriver.gecko.driver", "C:\\Project\\pageobjectinvitrotest\\Drivers\\geckodriver.exe");
+       driver = new FirefoxDriver();
+       driver.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
+       driver.manage().window().maximize();
+       driver.get("https://www.invitro.ru");
+       mainPage = new MainPage(driver);
+
+   }
 
     @Test
-    public void HealthServices(){
+    void succeedingTest () {
         RadiologyPage radiologyPage = mainPage.clickHealthServices();
         String heading = radiologyPage.GetHeadingText();
         Assert.assertEquals("Медицинские услуги в Москве", heading);
+
+
+
+    }
+
+    @AfterEach
+    void tearDown() {
+       driver.quit();
     }
 
 
-    @AfterAll
-    public void tearDown(){
-        driver.quit();
-    }
+
+
+
+
 
 
 }
